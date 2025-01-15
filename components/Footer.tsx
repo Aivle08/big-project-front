c2dded7858ac206ad614fdca43ff3fb7d28eb897
spaceOfSoul <ychh1123@gmail.com>
2025-01-15 13:51:25 +0900

    REFACTOR: div and p tags into styled components

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import Image from 'next/image'
+import { IconWrapper, UnderText, Wrapper } from './styles/footerStyled'
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col justify-center items-center w-full py-4">
+    <Wrapper>
         {/* 아이콘 영역 */}
-        <div className="w-[192px] h-[102px] flex flex-row justify-between items-center gap-[24px]">
+        <IconWrapper>
           <Image src="/instagram_icon.png" alt="Instagram" width={48} height={48} />
           <Image src="/linkedin_icon.png" alt="Linked In" width={48} height={48} />
           <Image src="/mail_icon.png" alt="Mail" width={48} height={48} />
-        </div>
+        </IconWrapper>
 
         {/* 텍스트 영역 */}
-        <p className="text-[#828282] font-normal text-[16px] leading-[22px]">
+        <UnderText>
           &copy; AIvler 08조 2025
-        </p>
-    </footer>
+        </UnderText>
+    </Wrapper>
   )
 }
 
