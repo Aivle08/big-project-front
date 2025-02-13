0c4e2e90dae1e80477d9827fd172121de748ca8f
ChaneHaDa <chane.ha.da@gmail.com>
2025-02-13 13:18:10 +0900

    update path

--- app/api/evaluationAPI.ts
+++ app/api/evaluationAPI.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 import { PassedResponse } from '../types/evaluation';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8080/api/v1';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://picks-up.site/api/v1';
 
 const evaluationAxiosInstance = axios.create({
   baseURL: API_BASE_URL,
